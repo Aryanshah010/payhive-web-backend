c7570b3fbe260fb2bbbbe7fd96a6d74c48c2cf38
Aryan shah <shaharyan844@gmail.com>
2026-01-29 11:37:40 +0545

    Completed CRUD for admin

--- src/routes/admin/admin.user.route.ts
+++ src/routes/admin/admin.user.route.ts
@@ -1,17 +1,17 @@
 import { Router } from "express";
 import { adminMiddleware, authorizedMiddleware } from "../../middlewares/authorized.middleware"
-import { Request, Response } from "express";
 import { AdminUserController } from "../../controllers/admin/admin.user.controller";
+import { uploads } from "../../middlewares/upload.middleware";
 
 let adminUserController = new AdminUserController();
 
 const router = Router();
 
 
-router.post("/", authorizedMiddleware, adminMiddleware, adminUserController.createUser);
+router.post("/", authorizedMiddleware, adminMiddleware, uploads.single("profilePicture"), adminUserController.createUser);
 router.get("/:id", authorizedMiddleware, adminMiddleware, adminUserController.getOneUser);
 router.get("/", authorizedMiddleware, adminMiddleware, adminUserController.getAllUsers);
-router.put("/:id", authorizedMiddleware, adminMiddleware, adminUserController.updateOneUser);
+router.put("/:id", authorizedMiddleware, adminMiddleware, uploads.single("profilePicture"), adminUserController.updateOneUser);
 router.delete("/:id", authorizedMiddleware, adminMiddleware, adminUserController.deleteOneUser);
 
 export default router;
